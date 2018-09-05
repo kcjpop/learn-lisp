5f1a3287745e7bff6316103bfcaeafb798a3081b
An Cao <kcj.pop.com@gmail.com>
2018-09-05 14:42:44 +0300

    P3: Reverse a list
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

---
+++ src/reverse-list.lisp
@@ -0,0 +1,23 @@
+;; L-99: Ninety-Nine Lisp Problems
+;; P3: Reverse a list
+(defpackage :kcjpop (:use :cl :1am))
+(in-package :kcjpop)
+
+(defun reverse-list-aux
+  (lst result)
+  (if (null lst)
+      result
+      (reverse-list-aux
+        (rest lst)
+        (cons (first lst) result))))
+
+(defun reverse-list
+  (lst)
+  (reverse-list-aux lst nil))
+
+;; Tests
+
+(test reverse-list-tests
+  (is (equal (reverse-list nil) nil))
+  (is (equal (reverse-list '(a)) '(a)))
+  (is (equal (reverse-list '(a b c)) '(c b a))))
